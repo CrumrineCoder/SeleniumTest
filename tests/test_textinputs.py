edc31f9228980dfa8275e0e595c9b68559335080
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-07-02 15:32:27 -0500

    Screenshot works

--- tests/test_textinputs.py
+++ tests/test_textinputs.py
@@ -10,4 +10,11 @@ class TestTextInputPage:
 
         value = input_page.get_normal_input_value()
         assert value == test_text
+    def test_failed_input(self, driver):
+        input_page = TextInputPage(driver)
+        test_text = "Hello World"
+        input_page.edit_normal_input(test_text)
+
+        value = input_page.get_normal_input_value()
+        assert value != test_text
 
